893c78910ed542d8730cfa04f01f0f69b0a61c3b
souptik4572 <souptiksarkar4572@gmail.com>
2020-08-01 10:15:19 +0530

    Adding Polygones, removing borders from shapes

--- maps.js
+++ maps.js
@@ -12,7 +12,7 @@ let arr = [];
 for (let k = 0; k < 10; k++) {
   arr.push([x1 + (k / 9) * (x2 - x1), y1 + (k * (y2 - y1)) / 9]);
 }
-console.log(arr);
+// console.log(arr);
 
 const distance = [
   296139.79,
@@ -30,7 +30,7 @@ let time_list = [];
 distance.forEach(function (element) {
   time_list.push(element / 72.0222);
 });
-console.log(time_list);
+// console.log(time_list);
 
 const velocity = [
   [85, 0.03],
@@ -49,13 +49,13 @@ for (let n = 0; n < 10; n++) {
   displacement.push(time_list[n] * velocity[n][1]);
   displacement[n] = displacement[n] * 9.231617281886954e-6;
 }
-console.log(displacement);
+// console.log(displacement);
 
 const m_list = [];
 for (let n = 0; n < 10; n++) {
   m_list.push(Math.tan(convertingToRadians(velocity[n][0] + 270)));
 }
-console.log(m_list);
+// console.log(m_list);
 for (let n = 0; n < 10; n++) {
   arr[n][0] =
     arr[n][0] - displacement[n] / Math.sqrt(1 + Math.pow(m_list[n], 2));
@@ -63,7 +63,7 @@ for (let n = 0; n < 10; n++) {
     arr[n][1] -
     m_list[n] * (displacement[n] / Math.sqrt(1 + Math.pow(m_list[n], 2)));
 }
-console.log(arr);
+// console.log(arr);
 
 const radius = [];
 for (let n = 0; n < 10; n++) {
@@ -75,7 +75,7 @@ for (let n = 0; n < 10; n++) {
     )
   );
 }
-console.log(radius);
+// console.log(radius);
 
 // Plotting the points inside a map and thus creating a polygon
 var mymap = L.map("mapid").setView([x1, y1], 13);
@@ -100,12 +100,14 @@ for (let n = 0; n < 10; n++) {
     fillColor: "#ed3e67",
     fillOpacity: 0.5,
     radius: radius[n] * 3,
+    stroke: false
   }).addTo(mymap);
   L.circle([arr[n][0], arr[n][1]], {
     color: 'red',
     fillColor: "#f03",
     fillOpacity: 0.5,
     radius: radius[n],
+    stroke: false
   }).addTo(mymap);
 }
 
@@ -114,8 +116,9 @@ for (let n = 0; n < 10; n++) {
 // // zoom the map to the polyline
 // mymap.fitBounds(polyline.getBounds());
 
-// Creating the Polyline
-// var polygon = L.polygon(arr).addTo(mymap);
+// Creating the PolyGone
+// const polyPoints = [[15.71181630638612, 81.4884415339953], [15.69368753553298, 81.47750367089544], [15.647317196289055, 81.55607147966352], [15.600784611203661, 81.63454312748541], [15.553799999769417, 81.71254695481029], [15.506974591902777, 81.79003646965961], [15.459956596285439, 81.86811050422611], [15.412737096187731, 81.94584609742401], [15.365132164373712, 82.02291953487587], [15.317330092572012, 82.10142859112081], [15.270176857220894, 82.17949905052181], [15.288281310828193, 82.19042224198684], [15.729945077239261, 81.49937939709515], [15.68352750211967, 81.57791870643844], [15.636957040907244, 81.65636750197237], [15.589943946803501, 81.7343541444841], [15.543099476217469, 81.81183215797921], [15.496071857728724, 81.88990038665568], [15.44886147234124, 81.96764147914823], [15.401275279638721, 82.04472622270781], [15.353501390650083, 82.12325228284966], [15.306385764435491, 82.20134543345186], [15.71181630638612, 81.4884415339953]]; 
+// var polygon = L.polygon(polyPoints, {color: 'green', fillColor: '#f03'}).addTo(mymap);
 
 // Creating the Circles
 // for(let i=0; i<lats.length; i++){
