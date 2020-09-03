f7f1c6134faaa882cbde27f70237e01c908003e1
Harsh Agarwal <harsh29092000@gmail.com>
2020-09-03 21:44:31 +0530

    Modified Database Schemas for better accuracy

--- gis_map/models.py
+++ gis_map/models.py
@@ -2,21 +2,31 @@ from django.db import models
 
 class InputLoc(models.Model):
 
-    # Latitude = models.DecimalField(max_digits=8, decimal_places=5)
-    # Longitude = models.DecimalField(max_digits=8, decimal_places=5)
-    Latitude = models.TextField()
-    Longitude = models.TextField()
-    Altitude = models.DecimalField(max_digits=8, decimal_places=3)
-    Speed = models.DecimalField(max_digits=6, decimal_places=3)
-    Direction = models.IntegerField()
-    Endurance = models.DecimalField(max_digits=6, decimal_places=3, default= 0.0)
     route_choices = [
         ('ES', 'Expanding Square'),
         ('PS', 'Parallel Sweep'),
         ('CS', 'Creeping Sweep')
     ]
+
+    latitude = models.FloatField(help_text= "Enter Latitude in Degrees", default= 0.0)
+    longitude = models.FloatField(help_text= "Enter Longitude in Degrees", default= 0.0)
+    altitude = models.FloatField(help_text= "Enter Altitude in feets", default= 0.0) # Unit: feets
+    speed = models.FloatField(help_text= "Enter Speed in knots", default= 0.0) # Unit: knots
+    direction = models.FloatField(help_text= "Enter Direction in Degrees", default= 0.0) # Unit: degrees
+    endurance = models.FloatField(help_text= "Enter Endurance in hours", default= 0.0) # Unit: hrs
+    divergence = models.FloatField(help_text= "Enter Divergence in Degrees", default= 0.0) # Unit: degrees
     search_choice = models.CharField(choices= route_choices,
     help_text= "Choose the search route to display on map", max_length= 30, default= 'ES')
 
     def __str__(self):
-        return f'''Last Known Position: ({self.Latitude},{self.Longitude})'''
+        return f"LKP: ({self.latitude},{self.longitude})"
+
+class SearchFacitily(models.Model):
+
+    name = models.CharField(max_length= 50)
+    contact = models.IntegerField(default= 0)
+    latitude = models.FloatField(default= 0.0)
+    longitude = models.FloatField(default= 0.0)
+
+    def __str__(self):
+        return self.name
