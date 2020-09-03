26ac99b045cc23650323be22040c5ec930fa0ad6
Harsh Agarwal <harsh29092000@gmail.com>
2020-09-03 22:43:34 +0530

    Added UserRegistration form

--- gis_map/forms.py
+++ gis_map/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import InputLoc
 
 class GisModelForm(forms.ModelForm):
     class Meta:
         model = InputLoc
         fields = '__all__'
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
