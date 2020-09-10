fa6b3861dbac153ec6070fe60defa30aef457867
Harsh Agarwal <harsh29092000@gmail.com>
2020-09-10 15:50:39 +0530

    Added Landing Page

--- gis_map/urls.py
+++ gis_map/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='gis-home'),
+    path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
     path('register/', views.register, name='register'),
 ]
