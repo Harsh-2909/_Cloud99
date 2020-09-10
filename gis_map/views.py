211626ea2abf59b297efb45e2fa2bc9938ff12a8
Harsh Agarwal <harsh29092000@gmail.com>
2020-09-10 15:52:01 +0530

    Added Search route map in models.py

--- gis_map/views.py
+++ gis_map/views.py
@@ -7,41 +7,16 @@ from . import utils
 import folium
 
 def home(request):
+    return render(request, 'gis_map/index.html')
+
+def search(request):
     
     m = folium.Map(location=[20.5937, 78.9629], width= '100%', height= '100%', zoom_start=5)
 
     form = GisModelForm(request.POST or None)
     if form.is_valid():
         instance = form.save()
-        
-        LKP, rad, polygon, sector, search, search_line = utils.cal([instance.latitude, instance.longitude], instance.speed, instance.altitude, instance.direction, instance.endurance)
-        search_coord = [search[0], search[1]]
-
-        m = folium.Map(location=LKP, tiles='Stamen Terrain', zoom_start= 5)
-        folium.Marker(location= LKP, tooltip="<i>Last Known Position</i>").add_to(m)
-        folium.Polygon(locations=polygon,
-                    popup='<b>Primary Search Zone</b>',
-                    color='#3186cc',
-                    fill=True,
-                    fill_color='#ff0000',
-                    fill_opacity= 0.8,
-                    stroke=False).add_to(m)
-        folium.Circle(location=LKP, radius= rad ,color='#3186cc', fill=False).add_to(m)
-        folium.Marker(location= search_coord,
-                    tooltip="<i>Nearest Search Facility. Click for details.</i>",
-                    popup= f'''<b>Coordinates: {search_coord}
-                    City: {search[2]}
-                    Contact Number: {search[3]}</b>''').add_to(m)
-
-        folium.PolyLine(locations= search_line,
-                    tooltip= "<i>Click for more info</i>",
-                    popup= "<b>Search route based on Expanding Square Search technique</b>").add_to(m)
-
-        folium.Polygon(locations=sector,
-                    popup='<b>Divergence Zone</b>',
-                    color='yellow',
-                    fill=False,
-                    fill_opacity= 0.8).add_to(m)
+        m = instance.ExpandingSquare()
         
     context = {
         'map': m._repr_html_(),
